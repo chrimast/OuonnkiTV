c07e9f88503cbf71733cd098cd9cb241a3766189
chrimast <119728939+chrimast@users.noreply.github.com>
2025-12-04 14:29:07 +0800

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -39,7 +39,7 @@ export default function Navigation() {
                 layoutId="logo-text"
                 className="hidden text-lg font-bold text-inherit md:block"
               >
-                OUONNKI TV
+                Freemovie TV
               </motion.p>
             </motion.div>
           </NavLink>
